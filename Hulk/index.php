97105c93dae6dc03c69331e46da63c1fa7df198b
argaaryadipa <73064867+argaaryadipa@users.noreply.github.com>
2021-08-06 23:34:17 +0800

    hulk

--- Hulk/index.php
+++ Hulk/index.php
@@ -18,10 +18,39 @@ include 'hulk.php';
 </head>
 <body>
 
-    <form method="post" action="">
-        <input type="text" name="input">
-        <button class="btn btn-primary" name="hulk" type="submit">Proses</button>
-    </form>
+    <div class="container mt-5">
+        <div class="card">
+            <h3 class="text-center">
+                Hulk
+            </h3>
+       
+
+            <div class="card-body">
+                <div class="row-lg mt-4">
+                    <div class="col-lg-5">
+                        <form method="post" action="">
+            
+                            <input type="text" name="input">
+                            <button class="btn btn-primary" name="hulk" type="submit">Proses</button>
+        
+                        </form>
+
+                        <h2>
+                            <?php
+                            foreach ($array as $arr) {
+                                echo $arr;
+                            }
+                            ?>
+                        </h2>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
